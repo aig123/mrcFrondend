33a15a88bed9c884c97a79d1cb48fbbc5cd12b37
aiguang <452661819@qq.com>
2018-04-23 08:51:16 +0800

    table

--- src/components/mrcForm/src/main.vue
+++ src/components/mrcForm/src/main.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <el-form :model="formData.data" label-width="80px" :rules="formData.rules" :ref="formData.name">
+    <el-form :model="formData.data" label-width="120px" :rules="formData.rules" :ref="formData.name">
       <div style="width: 100%;height: 20px"></div>
       <el-form-item :label="config.title" :key="config.field" v-for="config in formData.title" style="width: 95%;" :prop="config.field">
         <el-input v-model="formData.data[config.field]" v-if="config.type=='input'" :placeholder="config.placeholder"  style="width: 100%"></el-input>
@@ -8,11 +8,33 @@
           <el-option :label="item.name" :value="item.id" :key="item.id" v-for="item in config.data"></el-option>
         </el-select>
         <el-date-picker type="date" placeholder="选择日期" v-model="formData.data[config.field]" @change="dateChange"  v-if="config.type=='date'" style="width: 100%"></el-date-picker>
-
+        <el-date-picker
+          v-if="config.type=='daterange'"
+          v-model="formData.data[config.field]"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="config.startPlaceholder"
+          end-placeholder="config.endPlaceholder" style="width: 100%">
+        </el-date-picker>
+        <!--<el-upload-->
+          <!--v-if="config.type=='uploadFile'"-->
+          <!--class="upload-demo"-->
+          <!--:action="config.action"-->
+          <!--:on-preview="config.onPreviewFn"-->
+          <!--:on-remove="config.onRemoveFn"-->
+          <!--:before-remove="config.beforeRemove"-->
+          <!--:multiple="config.multiple",-->
+          <!--:limit="config.limit"-->
+          <!--:accept="config.accept"-->
+          <!--:on-exceed="config.onExceed"-->
+          <!--:file-list="config.fileList">-->
+          <!--<el-button size="small" type="primary">点击上传</el-button>-->
+          <!--<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>-->
+        <!--</el-upload>-->
+        <el-switch v-model="formData.data[config.delivery]" v-if="config.type=='switch'"></el-switch>
       </el-form-item>
-
       <el-form-item>
-        <el-button type="primary" @click="search(item.clickFn)" v-for="item in formData.button" :key="item.name">{{item.name}}</el-button>
+        <el-button type="primary" @click="search(item.clickFn)" v-for="item in formData.buttons" :key="item.name">{{item.name}}</el-button>
       </el-form-item>
     </el-form>
   </section>
--- src/pages/form-mrc.vue
+++ src/pages/form-mrc.vue
@@ -28,7 +28,7 @@
           :on-preview="handlePreview"
           :on-remove="handleRemove"
           :before-remove="beforeRemove"
-          multiple
+           multiple
           :limit="3"
           :on-exceed="handleExceed"
           :file-list="fileList">
@@ -66,10 +66,13 @@
         <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
+    <mrc-form v-model="formData" ref="mrcForm">
+    </mrc-form>
   </div>
 </template>
 
 <script>
+  import language  from "../language/language";
   export default {
     data() {
       return {
@@ -109,7 +112,92 @@
             { required: true, message: '请填写活动形式', trigger: 'blur' }
           ]
         },
-        fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+        fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+        formData:{
+          name:"form",
+          buttons:[{name:language.save,click:"save"},{name:language.save,click:"save"}],
+          title:[
+            {type: 'input', title: "活动名称", value: "", field: "name", placeholder: "请填写活动名称"},
+            {type: 'date', title: "开始时间", value: "", field: "date", placeholder: "请输入开始时间"},
+            {
+              type: 'daterange',
+              title: "活动时间范围",
+              startPlaceholder: "开始日期",
+              endPlaceholder: "结束日期",
+              value: "",
+              field: "value6",
+              placeholder: "请输入活动时间范围"
+            },
+            {
+              type: 'select',
+              title: "活动区域",
+              value: "",
+              field: "region",
+              placeholder: "请选择活动区域",
+              key: {label: "name", value: "id"},
+              data: [{id: 1, name: "区域一"}, {id: 2, name: "区域二"}]
+            },
+            {
+              type: 'uploadFile',
+              title: "上传附件",
+              value: "",
+              field: "comment",
+              placeholder: "请输入备注",
+              fileList: {
+                key: {name: "name", url: "url"},
+                data: [{
+                  name: 'food.jpeg',
+                  url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+                }]
+              },
+              action: "https://jsonplaceholder.typicode.com/posts/",
+              onPreviewFn: "handlePreview",
+              onRemoveFn: "handleRemove",
+              beforeRemoveFn: "beforeRemove",
+              multiple: "",
+              limit: "3",
+              onExceed: "handleExceed",
+              accept:""
+            },
+            {type: 'switch', title: "即时配送", value: "", field: "delivery"},
+          ],
+          data:{
+            name: '',
+            region: '',
+            date1: '',
+            date2: '',
+            delivery: false,
+            type: [],
+            resource: '',
+            desc: '',
+            value6:"",
+            date:""
+          },
+          rules: {
+            name: [
+              { required: true, message: '请输入活动名称', trigger: 'blur' },
+              { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+            ],
+            region: [
+              { required: true, message: '请选择活动区域', trigger: 'change' }
+            ],
+            date1: [
+              { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+            ],
+            date2: [
+              { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+            ],
+            type: [
+              { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+            ],
+            resource: [
+              { required: true, message: '请选择活动资源', trigger: 'change' }
+            ],
+            desc: [
+              { required: true, message: '请填写活动形式', trigger: 'blur' }
+            ]
+          },
+        },
       };
     },
     methods: {
