2ab67019b300197e3e629d1c13366bd6f1c48252
aiguang <452661819@qq.com>
2018-05-08 08:54:51 +0800

    table

--- build/buildC.js
+++ build/buildC.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const vueLoaderConfig = require('./vue-loader.conf')
-var failPlugin = require('webpack-fail-plugin');
 const utils = require('./utils')
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -12,9 +11,6 @@ webpack( {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js',
   },
-  plugins: [
-    failPlugin
-  ],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     // alias: {
@@ -23,6 +19,48 @@ webpack( {
     //   'nb': path.resolve(__dirname, 'http://vmccsyswfdeweb.faw-vw.in:9000/JsNameSpace.jsx')
     // }
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  }
 
 }, (err, stats) => {
   if (err) throw err
